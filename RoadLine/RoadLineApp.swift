a810595a45adcff37f16c09da71c24286ababc35
shaelsien630 <shaelsien630@gmail.com>
2024-11-05 16:06:08 +0900

    Initial Commit

---
+++ RoadLine/RoadLineApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoadLineApp.swift
+//  RoadLine
+//
+//  Created by 최서희 on 11/5/24.
+//
+
+import SwiftUI
+
+@main
+struct RoadLineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
